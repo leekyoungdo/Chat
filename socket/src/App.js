f0a3fe6c848de496dfc4dd09057843ebf6242bb6
Kyoung Do Lee <kyoungdolee@Kyoungui-MacBookPro.local>
2023-12-21 17:55:12 +0900

    test

---
+++ socket/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Chatting from "./component/Chatting";
+
+function App() {
+  return (
+    <div>
+      <Chatting></Chatting>
+    </div>
+  );
+}
+
+export default App;
